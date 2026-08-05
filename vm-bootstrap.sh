c771f2f2c5c3311191278505302b2b58bc12515a
William Birklaw <262987797+Birklaw@users.noreply.github.com>
2026-08-05 20:01:36 +0100

    feat(vm-bootstrap): Add Dev Containers VS Code Extension to bootstrap

--- vm-bootstrap.sh
+++ vm-bootstrap.sh
@@ -62,6 +62,9 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf install -y code
 
+# ---------- VS Code: Dev Containers extension (required for DevPod) ----------
+code --install-extension ms-vscode-remote.remote-containers
+
 # ---------- mise + shared toolset ----------
 bash "$REPO_ROOT/template/setup-mise.sh"
 
